1ab16194a4d0a53a302c662216bb1ac18473c204
Liron Amar <liron.amar@post.runi.ac.il>
2026-01-10 23:41:16 +0200

    attempt 6

--- Network.java
+++ Network.java
@@ -152,10 +152,10 @@ public class Network {
 
     // Returns a textual description of all the users in this network, and who they follow.
     public String toString() {
-       String ans = "Network:\n";
+       String ans = "Network:";
        
        for (int i = 0; i < userCount; i++) {
-           ans = ans + users[i].toString() + "\n";
+           ans = "\n"+ ans + users[i].toString() + " ";
        }
        return ans;
     }
